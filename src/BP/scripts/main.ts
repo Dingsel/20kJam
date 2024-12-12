307229efa8dfee2330500d16d6a101883598ec43
Pablo <fabio.eggert2009@gmail.com>
2024-12-12 21:40:13 +0100

    firstPart Intro

--- src/BP/scripts/main.ts
+++ src/BP/scripts/main.ts
@@ -1,1 +1,11 @@
 console.warn("[cmca] If You See This, You Managed To Setup The Boilerplate. Happy Coding! ")
+
+import { world } from "@minecraft/server"
+import { ActionFormData } from "@minecraft/server-ui"
+
+world.getAllPlayers().forEach(player => {   
+const ui = new ActionFormData()
+.title('RT20K.intro')
+.button('')
+.show(player)
+})
